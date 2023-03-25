9081597acd6b18874ba28cc90b75ffdefbecad83
Bruce Jenner <Bruce.Jenner@gmail.com>
2023-03-24 20:52:47 -0700

    Cleaned up the tests a bit...still failing but runs

--- test_smoothrainbow.py
+++ test_smoothrainbow.py
@@ -1,16 +1,18 @@
 import unittest
-import rainbow
+import smoothrainbow
 from neopixel import Neopixel
 
 class TestRainbowRole(unittest.TestCase):
     def setUp(self):
+        global Neopixel
         # Save the original Neopixel class and replace it with our custom class
-        self.original_neopixel = rainbow.Neopixel
-        rainbow.Neopixel = self.CustomNeopixel
+        self.original_neopixel = Neopixel
+        Neopixel = self.CustomNeopixel
 
     def tearDown(self):
+        global Neopixel
         # Restore the original Neopixel class
-        rainbow.Neopixel = self.original_neopixel
+        Neopixel = self.original_neopixel
 
     class CustomNeopixel(Neopixel):
         def __init__(self, *args, **kwargs):
@@ -26,21 +28,21 @@ class TestRainbowRole(unittest.TestCase):
 
     def test_rainbow_role(self):
         # Limit the number of iterations for the test
-        rainbow.iterations_limit = 10
+        smoothrainbow.iterations_limit = 10
 
         # Run the rainbow_role function
-        rainbow.rainbow_role()
+        smoothrainbow.rainbow_role()
 
         # Instantiate the expected Neopixel instance
         expected_neopixel = self.CustomNeopixel(50, 0, 28, "GRB")
 
         # Check if the fill and show methods were called the expected number of times
-        self.assertEqual(len(expected_neopixel.fill_calls), rainbow.iterations_limit)
-        self.assertEqual(expected_neopixel.show_calls, rainbow.iterations_limit)
+        self.assertEqual(len(expected_neopixel.fill_calls), smoothrainbow.iterations_limit)
+        self.assertEqual(expected_neopixel.show_calls, smoothrainbow.iterations_limit)
 
         # Check if the hue value was incremented by 150 on each iteration
         hue_values = [call[0] for call in expected_neopixel.fill_calls]
-        expected_hue_values = list(range(0, 150 * rainbow.iterations_limit, 150))
+        expected_hue_values = list(range(0, 150 * smoothrainbow.iterations_limit, 150))
         self.assertEqual(hue_values, expected_hue_values)
 
 if __name__ == '__main__':
