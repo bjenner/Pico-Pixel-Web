24e0c06f59efe1adbf6578d2a1c656b7a5491b7c
Bruce Jenner <Bruce.Jenner@gmail.com>
2023-03-24 19:55:30 -0700

    Adds unit tests for pico_status written by GPT-4

---
+++ test_pico_status.py
@@ -0,0 +1,19 @@
+import unittest
+from pico_status import PicoStatus
+
+class TestPicoStatus(unittest.TestCase):
+
+    def test_temperature(self):
+        # Test temperature method
+        temp_c = PicoStatus.temperature()
+        self.assertIsNotNone(temp_c)
+        self.assertTrue(-40 <= temp_c <= 80)
+
+    def test_memory(self):
+        # Test memory method
+        mem = PicoStatus.memory()
+        self.assertIsNotNone(mem)
+        self.assertTrue(mem > 0)
+
+if __name__ == '__main__':
+    unittest.main()
