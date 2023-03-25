f217ab6b39ede7eae0a52eec7a644938fe5a1c76
Bruce Jenner <Bruce.Jenner@gmail.com>
2023-03-24 20:45:05 -0700

    Second attempt by GPT-4 with no unittest.mock

--- test_smoothrainbow.py
+++ test_smoothrainbow.py
@@ -1,32 +1,45 @@
 import unittest
-from unittest.mock import Mock, call
-from rainbow import rainbow_role
+import rainbow
+from neopixel import Neopixel
 
 class TestRainbowRole(unittest.TestCase):
+    def setUp(self):
+        # Save the original Neopixel class and replace it with our custom class
+        self.original_neopixel = rainbow.Neopixel
+        rainbow.Neopixel = self.CustomNeopixel
+
+    def tearDown(self):
+        # Restore the original Neopixel class
+        rainbow.Neopixel = self.original_neopixel
+
+    class CustomNeopixel(Neopixel):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fill_calls = []
+            self.show_calls = 0
+
+        def fill(self, color):
+            self.fill_calls.append(color)
+
+        def show(self):
+            self.show_calls += 1
 
     def test_rainbow_role(self):
-        # Mock the Neopixel class
-        neopixel_mock = Mock()
-        
-        # Replace Neopixel import in rainbow module with the mock
-        rainbow.Neopixel = neopixel_mock
-
         # Limit the number of iterations for the test
         rainbow.iterations_limit = 10
 
         # Run the rainbow_role function
-        rainbow_role()
+        rainbow.rainbow_role()
 
-        # Check if the Neopixel class was instantiated
-        neopixel_mock.assert_called_once_with(50, 0, 28, "GRB")
+        # Instantiate the expected Neopixel instance
+        expected_neopixel = self.CustomNeopixel(50, 0, 28, "GRB")
 
         # Check if the fill and show methods were called the expected number of times
-        neopixel_instance = neopixel_mock.return_value
-        self.assertEqual(neopixel_instance.fill.call_count, rainbow.iterations_limit)
-        self.assertEqual(neopixel_instance.show.call_count, rainbow.iterations_limit)
+        self.assertEqual(len(expected_neopixel.fill_calls), rainbow.iterations_limit)
+        self.assertEqual(expected_neopixel.show_calls, rainbow.iterations_limit)
 
         # Check if the hue value was incremented by 150 on each iteration
-        hue_values = [call.args[0] for call in neopixel_instance.colorHSV.call_args_list]
+        hue_values = [call[0] for call in expected_neopixel.fill_calls]
         expected_hue_values = list(range(0, 150 * rainbow.iterations_limit, 150))
         self.assertEqual(hue_values, expected_hue_values)
 
