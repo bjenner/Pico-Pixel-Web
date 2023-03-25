0c311c1e565881853f036af14aaf23847a6af637
Bruce Jenner <Bruce.Jenner@gmail.com>
2023-03-24 20:40:18 -0700

    Updated smoothrainbow.py as recommended by GPT-4 to support unit tests

--- smoothrainbow.py
+++ smoothrainbow.py
@@ -2,17 +2,27 @@ import time
 from neopixel import Neopixel
 from mylogging import safe_print
 
+# Add a variable to limit the number of iterations
+iterations_limit = None
+
 def rainbow_role():
     numpix = 50
     strip = Neopixel(numpix, 0, 28, "GRB")
 
     hue = 0
+    iterations = 0
     while(True):
         color = strip.colorHSV(hue, 255, 100)
         strip.fill(color)
         strip.show()
         time.sleep(0.01)
         hue += 150
+
+        # Increment the iterations and break the loop if the limit is reached
+        if iterations_limit is not None:
+            iterations += 1
+            if iterations >= iterations_limit:
+                break
     
 if __name__ == "__main__":
     safe_print("smooth rainbow testing...")
@@ -21,5 +31,3 @@ if __name__ == "__main__":
 
 else:
     safe_print( __name__ + " imported")
-
-
